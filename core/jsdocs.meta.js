9bca57beced283ce2adb803dc3a3c58258ea92a9
Leonardo Serrano <vageyenaman@gmail.com>
2017-07-29 15:03:35 -0700

    Adds jsdocs.meta.js, for defining miscellaneous metadata and typedefs

---
+++ core/jsdocs.meta.js
@@ -0,0 +1,9 @@
+/*
+    This file may contain miscellaneous jsdocs type definitions as required
+*/
+
+/**
+ * @typedef {Object} Point
+ * @property {number} xPos - The x coordinate
+ * @property {number} yPos - The y coordinate
+ */
